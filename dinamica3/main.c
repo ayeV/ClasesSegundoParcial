370145e7209cb9a8ae95ff0471d2e90f0677840e
ayeV <36230640+ayeV@users.noreply.github.com>
2018-05-21 10:47:31 -0300

    Add files via upload

--- dinamica3/main.c
+++ dinamica3/main.c
@@ -2,42 +2,58 @@
 #include <stdlib.h>
 #define TAM 5
 int main()
-{    int i;
+{
+    int i;
     int* vec;
     int* aux;
     vec = (int*) malloc(TAM*sizeof(int));
-    if(vec == NULL){
+    if(vec == NULL)
+    {
         printf("\nNo se pudo conseguir memoria\n");
         system("pause");
         exit(1);
     }
-    for(i=0;i<TAM;i++){
+    for(i=0; i<TAM; i++)
+    {
         printf("Ingrese nro: ");
         scanf("%d",vec+i);//scanf recibe dir de memoria
     }
 
- for(i=0;i<TAM;i++){
+    for(i=0; i<TAM; i++)
+    {
         printf("%d\n",*(vec+i)); //printf recibe valores
 
     }
     //si quisiera agrandar el vector:
 
     aux = (int*)realloc(vec,(TAM+5)*sizeof(int));
-    if(aux== NULL){
+    if(aux== NULL)
+    {
         printf("No se pudo conseguir memoria \n");
         system("pause");
         exit(1);
     }
-    else{
+    else
+    {
         vec = aux;
-        for(i=5;i<TAM+5;i++){
-        printf("Ingrese nro: ");
-        scanf("%d",vec+i);
-    }
+        for(i=5; i<TAM+5; i++)
+        {
+            printf("Ingrese nro: ");
+            scanf("%d",vec+i);
+        }
     }
 
- for(i=0;i<TAM+5;i++){
+    for(i=0; i<TAM+5; i++)
+    {
         printf("%d\n",*(vec+i));
+    }
+    //si quisiera achicar espacio
+
+    vec = (int*) realloc(vec,TAM*sizeof(int));
+    for(i=0; i<TAM; i++)
+    {
+        printf("%d\n",*(vec+i));
+
     }
 
 
