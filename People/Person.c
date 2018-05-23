4996f9c4f27b3ecc78ae2d4dc4ccd012e9b81bd1
ayeV <36230640+ayeV@users.noreply.github.com>
2018-05-23 11:12:01 -0300

    Add files via upload

---
+++ People/Person.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include "Person.h"
+
+
+Person* person_new (int age, int something)
+{
+    Person* this = malloc(sizeof(Person));
+    if(this != NULL)
+        initialize(this,age,something);
+
+    return this;
+}
+
+/** \brief  Inicializa a una persona recibida como parametro
+ * \param Person* this Puntero a la persona
+ * \param int age Edad de la persona
+ * \param int something Otros datos
+ * \return void
+ *
+ */
+static void initialize(Person* this, int age, int something)
+{
+    this->age = age;
+    this->something = something;
+}
+
+/** \brief  Setea la edad de una persona recibida como parametro
+ * \param Person* this Puntero a la persona
+ * \param int age Edad de la persona
+ * \return void
+ *
+ */
+void person_setAge(Person* this, int age)
+{
+    if(age > 0)
+        this->age = age;
+}
+
+void person_setAlgo(Person* this, int algo)
+{
+    if(algo > 0)
+        this->something = algo;
+}
+
+
+/** \brief Obtiene la edad de una persona recibida como parametro
+ * \param Person* this Puntero a la persona
+ * \return int age Edad de la persona
+ *
+ */
+int person_getAge(Person* this)
+{
+    return this->age;
+}
+
+int person_getAlgo(Person* this)
+{
+    return this->something;
+}
+
+
+/** \brief Libera el espacio ocupado por una persona recibida como parametro
+ * \param Person* this Puntero a la persona
+ * \return void
+ *
+ */
+void person_free(Person * this) {
+    // Do any other freeing required here.
+    free(this);
+}
---
+++ People/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Person.h"
+
+
+int main()
+{
+    Person* personArray[10];
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+        personArray[i] = person_new(i,i);
+    }
+    for(i = 0; i < 10; i++)
+
+    {
+        printf("Edad: ");
+        scanf("%d",&(*(personArray+i))->age);
+        person_setAge(personArray[i],i-4);
+        printf("Algo: ");
+        scanf("%d",&(*(personArray+i))->something);
+        person_setAlgo(personArray[i],i-4);
+    }
+    for(i = 0; i < 10; i++)
+    {
+        printf("\nAge: %2d",person_getAge(personArray[i]));
+        printf("\nAlgo: %2d",person_getAlgo(personArray[i]));
+    }
+    scanf(" ");
+
+
+    free(*personArray);
+    return 0;
+}
+
